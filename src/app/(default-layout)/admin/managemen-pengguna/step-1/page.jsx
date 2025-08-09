671e1a2141b294c4de0ef8fa8d81d881726bc45e
Ruumi9 <asepskizo27@gmail.com>
2025-08-09 20:51:16 +0700

    add managemen-pengguna step 1,2,3

--- src/app/(default-layout)/admin/managemen-pengguna/step-1/page.jsx
+++ src/app/(default-layout)/admin/managemen-pengguna/step-1/page.jsx
@@ -1,57 +1,139 @@
 'use client';
 import TransitionLink, { navigateTo } from "@/components/TransitionLink";
+import { useState } from "react";
 
 function ManagemenPenggunaStep1Page() {
+    const [RadioValue, setRadioValue] = useState('false');
+    const handleRadioChange = (event) => {
+        setRadioValue(event.target.value);
+    };
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
         // handle submit here
-        navigateTo('step-6');
+        navigateTo('step-2');
     }
+
     return (
-        <div data-transition-page data-animate="enter" className="pr-8 py-4 pl-14 w-full gap-x-3 gap-y-2 grid grid-cols-[auto_1fr] max-md:grid-cols-1 fade-in-up fade-out-up">
-            <div className="grid col-span-full grid-cols-4">
-                <div className="items-center justify-center hidden md:flex md:col-span-1">
-                    <div className="border-secondary border-2 w-full bg-base-100 shadow-sm transition-all hover:-translate-y-0.5 active:translate-y-0 cursor-pointer card">
-                        <img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="Ikon 2" className="h-38 mt-3 mx-3 rounded-md object-cover" />
-                        <div className="items-center text-center card-body">
-                            <h2 className="card-title">Manajemen Pengguna</h2>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-body col-span-4 md:col-span-3 w-full">
-                    <TransitionLink href={'managemen-pengguna/step-1'} className=" transition-all hover:-translate-y-0.5 active:translate-y-0 cursor-pointer border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">1. DAFTAR PENGGUNA DAN HAK AKSES DIREKTORAT/DIVISI SDM</h3>
-                    </TransitionLink>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">2. DAFTAR NAMA PENGGUNA DAN HAK AKSES</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">&nbsp;    </h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">4. AKUNTABILITAS PENGGUNA - MODUL LAYANAN MANDIRI PEKERJA</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">5. AKUNTABILITAS PENGGUNA - MODUL DESAIN ORGANISASI</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">6. AKUNTABILITAS PENGGUNA - MODUL REKRUTMENT DAN SELEKSI</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">7. AKUNTABILITAS PENGGUNA - MODUL HUBNGAN DAN INDUSTRIAL</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">8. AKUNTABILITAS PENGGUNA - MODUL KOMPENSASI DAN BENEFIT</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">9. AKUNTABILITAS PENGGUNA - MODUL ASESMEN</h3>
-                    </div>
-                    <div className="border px-4 border-secondary/90 py-0 rounded-4xl">
-                        <h3 className="text-lg text-secondary font-normal">10. AKUNTABILITAS PENGGUNA - MODUL MANAJEMEN KINERJA</h3>
-                    </div>
+        <form onSubmit={handleFormSubmit} data-transition-page data-animate="enter" className="gap-4 grid grid-cols-1 @max-6xl/main:grid-cols-1 px-8 py-4 fade-in-up fade-out-up">
+            <h1 className="col-span-full font-semibold text-3xl flex gap-2">DAFTAR PENGGUNA DAN HAK AKSES DIREKTORAT SDM</h1>
+            <div className="flex flex-col w-full gap-2">
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        <tbody>
+
+                            <tr>
+                                <td className="font-bold">NO</td>
+                                <td className="font-bold">NAMA</td>
+                                <td className="font-bold">EMAIL</td>
+                                <td className="font-bold">HAK AKSES</td>
+                                <td className="font-bold">STATUS</td>
+                            </tr>
+                            <tr>
+                                <td>1.</td>
+                                <td>ABC</td>
+                                <td>emalia@peoplepatner.id</td>
+                                <td>MASTER ADMIN</td>
+                                <td>
+                                    <select
+                                        name=""
+                                        onChange={(e) => {
+                                            if (e.target.value === 'aktif') {
+                                                e.target.classList.add('bg-success');
+                                                e.target.classList.remove('bg-gray-500');
+                                            } else {
+                                                e.target.classList.remove('bg-success');
+                                                e.target.classList.add('bg-gray-500');
+                                            }
+                                        }}
+                                        className="select text-white bg-success"
+                                    >
+                                        <option value="aktif">Aktif</option>
+                                        <option value="non-aktif">Non Aktif</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>DEF</td>
+                                <td>emalia@peoplepatner.id</td>
+                                <td>MASTER ADMIN</td>
+                                <td>
+                                    <select
+                                        name=""
+                                        onChange={(e) => {
+                                            if (e.target.value === 'aktif') {
+                                                e.target.classList.add('bg-success');
+                                                e.target.classList.remove('bg-gray-500');
+                                            } else {
+                                                e.target.classList.remove('bg-success');
+                                                e.target.classList.add('bg-gray-500');
+                                            }
+                                        }}
+                                        className="select text-white bg-success"
+                                    >
+                                        <option value="aktif">Aktif</option>
+                                        <option value="non-aktif">Non Aktif</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td>GHI</td>
+                                <td>emalia@peoplepatner.id</td>
+                                <td>ADMIN LAYANAN MANDIRI PEKERJA</td>
+                                <td>
+                                    <select
+                                        name=""
+                                        onChange={(e) => {
+                                            if (e.target.value === 'aktif') {
+                                                e.target.classList.add('bg-success');
+                                                e.target.classList.remove('bg-gray-500');
+                                            } else {
+                                                e.target.classList.remove('bg-success');
+                                                e.target.classList.add('bg-gray-500');
+                                            }
+                                        }}
+                                        className="select text-white bg-success"
+                                    >
+                                        <option value="aktif">Aktif</option>
+                                        <option value="non-aktif">Non Aktif</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>4.</td>
+                                <td>JKL</td>
+                                <td>emalia@peoplepatner.id</td>
+                                <td>ADMIN DESAIN ORGANISASI</td>
+                                <td>
+                                    <select
+                                        name=""
+                                        onChange={(e) => {
+                                            if (e.target.value === 'aktif') {
+                                                e.target.classList.add('bg-success');
+                                                e.target.classList.remove('bg-gray-500');
+                                            } else {
+                                                e.target.classList.remove('bg-success');
+                                                e.target.classList.add('bg-gray-500');
+                                            }
+                                        }}
+                                        className="select text-white bg-success"
+                                    >
+                                        <option value="aktif">Aktif</option>
+                                        <option value="non-aktif">Non Aktif</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </div>
+            <div className="col-span-full flex gap-2 justify-end">
+                <TransitionLink href="/admin" className="btn btn-info">Kembali</TransitionLink>
+                <button type="submit" className="btn btn-success">Simpan & Lanjut</button>
+            </div>
+        </form>
     );
 }
 
