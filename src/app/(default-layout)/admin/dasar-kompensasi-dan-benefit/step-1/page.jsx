a4c84adf75b38da7107c2162454be706473e95d5
Ruumi9 <asepskizo27@gmail.com>
2025-08-18 20:34:37 +0700

    fixing some layout in dasar kompensasi dan benefit step-1

--- src/app/(default-layout)/admin/dasar-kompensasi-dan-benefit/step-1/page.jsx
+++ src/app/(default-layout)/admin/dasar-kompensasi-dan-benefit/step-1/page.jsx
@@ -11,14 +11,14 @@ function DasarKompensasiDanBenefitStep1Page() {
 
     return (
         <form onSubmit={handleFormSubmit} data-transition-page data-animate="enter" className="gap-4 grid grid-cols-1 @max-6xl/main:grid-cols-1 px-8 py-4 fade-in-up fade-out-up">
-            <h1 className="col-span-full font-semibold text-3xl flex gap-2">STRUKTUR BENEFIT</h1>
+            {/* <h1 className="col-span-full font-semibold text-3xl flex gap-2">STRUKTUR BENEFIT</h1> */}
             <div className="flex flex-col w-full gap-2">
                 <div className="overflow-x-auto w-full">
                     <table className="table table-fixed w-full min-w-[500px]">
                         <tr>
-                            <td className="font-bold" colSpan={2}>Komponen Gaji</td>
-                            <td className="font-bold" colSpan={2}>Status Pajak</td>
-                            <td className="font-bold" colSpan={2}>PTKP</td>
+                            <td className="font-bold text-3xl" colSpan={2}>Komponen Gaji</td>
+                            <td className="font-bold text-3xl" colSpan={2}>Status Pajak</td>
+                            <td className="font-bold text-3xl" colSpan={2}>PTKP</td>
                         </tr>
                         <tr>
                             <td className="">1. Gaji Pokok</td>
